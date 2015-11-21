b245b9c2e144e7593a00e3aa1a59e1de1f1c8864
A B M Ruman <abm.ruman@gmail.com>
2015-11-21 19:07:10 +0600

    Add setFullScreen method.

--- app/src/main/java/com/androidApps/ruman/dx_ball/MainActivity.java
+++ app/src/main/java/com/androidApps/ruman/dx_ball/MainActivity.java
@@ -13,16 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         gameView = new GameView(this);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        gameView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        setFullScreen();
         setContentView(gameView);
 
     }
@@ -30,6 +21,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onRestart() {
         super.onRestart();
+        setFullScreen();
+    }
+
+    public void setFullScreen() {
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         gameView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
