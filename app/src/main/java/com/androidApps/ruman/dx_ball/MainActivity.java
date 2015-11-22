641c516893b6e408f259bf0c86ca400aedfc11e7
A B M Ruman <abm.ruman@gmail.com>
2015-11-23 01:51:48 +0600

    Move setFullScreen method to GameView.

--- app/src/main/java/com/androidApps/ruman/dx_ball/MainActivity.java
+++ app/src/main/java/com/androidApps/ruman/dx_ball/MainActivity.java
@@ -2,7 +2,6 @@ package com.androidApps.ruman.dx_ball;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -14,26 +13,21 @@ public class MainActivity extends Activity {
         Game.dxBall = new DxBall();
         Game.gameView = new GameView(this);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setFullScreen();
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        AddListeners();
         setContentView(Game.gameView);
+    }
+
+    private void AddListeners() {
+
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        setFullScreen();
     }
 
-    public void setFullScreen() {
-        this.getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        Game.gameView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-    }
+
 }
